e14cc257bbd65b20f2b903e70ad25f05f236be2f
Amansimlembe <amansimlembe11@gmail.com>
2025-03-23 19:23:03 +0300

    Update react icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { FaHome, FaBriefcase, FaComments, FaUser } from 'react-icons/fa';
+import { FaHome, FaBriefcase, FaComments, FaUser } from 'react-icons/fa'; // Correct import
 import LoginScreen from './screens/LoginScreen';
 import JobSeekerScreen from './screens/JobSeekerScreen';
 import EmployerScreen from './screens/EmployerScreen';
