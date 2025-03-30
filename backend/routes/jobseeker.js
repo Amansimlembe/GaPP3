65b832360190a516c45f29c2cd24bd062465c116
Amansimlembe <amansimlembe11@gmail.com>
2025-03-30 15:57:59 +0300

    Add redis module to backend

--- backend/routes/jobseeker.js
+++ backend/routes/jobseeker.js
@@ -4,9 +4,7 @@ const User = require('../models/User');
 const Job = require('../models/Job');
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
-const { jobMatcher } = require('../utils/jobMatcher');
-const { router: authRoutes, authMiddleware } = require('./routes/auth');
-
+const { jobMatcher } = require('../utils/jobMatcher')
 
 
 
