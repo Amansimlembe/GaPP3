3cf23645929f1c527685d091af2cb97fb16a589b
Amansimlembe <amansimlembe11@gmail.com>
2025-06-21 15:41:29 +0300

    all

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import './tailwind.config.js'; // Ensure Tailwind is imported
+import '../tailwind.config.js'; // Ensure Tailwind is imported
 
 
 ReactDOM.render(
